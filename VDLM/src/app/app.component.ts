41062c9417eb4eec1bc2de7d9541054d9761938a
NevaByteTV <appsnevy@gmail.com>
2023-02-18 16:50:25 +0100

    delete

--- VDLM/src/app/app.component.ts
+++ VDLM/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'VDLM';
+  loggeado: boolean = true;
+
+  public nombre_usuario = localStorage.setItem('nombre_usuario','Raul');
+  public apellido1 = localStorage.setItem('apellido1','Hernandez');
+  public apellido2 = localStorage.setItem('apellido2','Saez');
+
+  constructor (public router: Router) {
+
+    if(this.loggeado == true && this.router.url == "/login") {
+      this.router.navigate(['/']);
+    } 
+
+  }
+
 }
-
-
